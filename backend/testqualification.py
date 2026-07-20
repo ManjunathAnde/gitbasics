7c76844f96dd0b43696aeef16388d262e04032f3
ManjunathAnde <manjunathande7@gmail.com>
2026-07-20 22:50:42 +0530

    Retry condition

--- backend/testqualification.py
+++ backend/testqualification.py
@@ -1,6 +1,8 @@
 score_user=int(input("Enter your score: "))
 if score_user < 0 or score_user > 100:
     raise ValueError("Score must be between 0 and 100")
+if score_user>60 and score_user<75:
+    print("You are eligible to retry the exam")
 if score_user >= 90:
     print("You are qualified for the next round")
 else:
