b3a8913cf4e00b74d29046f771f59e4376710cf2
ManjunathAnde <manjunathande7@gmail.com>
2026-07-21 17:10:50 +0530

    up to date local copy

--- backend/testqualification.py
+++ backend/testqualification.py
@@ -6,8 +6,5 @@ if score_user>60 and score_user<75:
 if score_user >= 90:
     print("You are qualified for the next round")
 else:
-<<<<<<< HEAD
-    print("You are faileddd44")
-=======
+
     print("You are not qualified")
->>>>>>> test-results
